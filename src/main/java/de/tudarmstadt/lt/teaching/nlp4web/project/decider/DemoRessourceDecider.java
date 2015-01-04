a7008c90eff6ed2f7581c6cc5e2c082bea3b8998
Martin Hellwig <martinhellwig@gmx.de>
2015-01-04 19:44:39 +0100

    Add something in DemoDecider

--- src/main/java/de/tudarmstadt/lt/teaching/nlp4web/project/decider/DemoRessourceDecider.java
+++ src/main/java/de/tudarmstadt/lt/teaching/nlp4web/project/decider/DemoRessourceDecider.java
@@ -3,13 +3,26 @@ package de.tudarmstadt.lt.teaching.nlp4web.project.decider;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasConsumer_ImplBase;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+
+import de.tudarmstadt.lt.teaching.nlp4web.project.objects.QuestionObject;
+import de.tudarmstadt.lt.teaching.nlp4web.project.objects.RightAnswer;
+import de.tudarmstadt.ukp.teaching.general.type.Question;
 import de.tudarmstadt.ukp.teaching.general.type.Result;
 
 public class DemoRessourceDecider extends JCasConsumer_ImplBase {
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		QuestionObject question;
+		for (Question q : JCasUtil.select(jcas, Question.class)) { 
+			question = new QuestionObject(q.getQuestion(), q.getAnswer1(), q.getAnswer2(), q.getAnswer3(), 
+					q.getAnswer4(), RightAnswer.valueOf(q.getRightAnswer()));
+		}
+		
+		
+		
 		Result result = new Result(jcas);
 		result.setRessouceType("Demo");
 		result.setAnswer1Possibility(0.45f);
